74debfd85bbaf36703c5c1bb9eead535c2466614
10916720@live.mercer.edu <hailey*123>
2014-09-04 10:37:53 -0400

    Adding week 3 homework

---
+++ exercises/Class_6/grade_calculator_infinite.rb
@@ -0,0 +1,16 @@
+puts "give me your grades, one at a time. Type \"done\" when you are done."
+grades = [] # grades = Array.new
+while true
+    grade = gets.chomp
+    if (grade.downcase == 'done')
+        exit
+    else
+        grades.push(grade.to_i)
+    end
+end
+
+totatl points = 0
+grades.each do |grade|
+    total_points = total points + grade
+end 
+puts "my average = " + (total_points / grade.size).to_s
