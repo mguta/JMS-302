9c841ec4ba1c0c86edda586afbd01d024ceb9170
10916720@live.mercer.edu <hailey*123>
2014-10-12 20:49:24 -0400

    week_8
    hw

---
+++ midterm/election.rb
@@ -0,0 +1,19 @@
+
+
+candidate_1 = "Michelle Nunn"
+candidate_2 = "David Perdue
+
+
+puts " What is the unlimited number for each candidate until you type 'done'"
+    reply= gets.chomp
+    if (reply.downcase == 'done')
+        break
+    rep
+end 
+
+gets_results(name)
+    results = 0
+    percent_of_vote = x/((x+y)*100).to_f
+
+prints " #{candidate} #{percent_of_vote}
+
